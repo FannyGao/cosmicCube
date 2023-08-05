178b35d6bc37d87fbfd6ee4da871337d594af5cf
dafengxiang <1205679862@qq.com>
2023-08-05 20:49:39 +0800

    feat: 引入element组件库

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
+/*
+ * @Description: 渲染线程入口文件
+ * @Author: wangfengxiang
+ * @Date: 2023-08-05 16:32:42
+ * @LastEditTime: 2023-08-05 20:46:43
+ * @LastEditors: wangfengxiang
+ */
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+createApp(App).use(ElementPlus).mount('#app')
